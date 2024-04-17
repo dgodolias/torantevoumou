4ab8c3af024e48c65bea854c23d9903d57c8eaeb
dgodolias <dgodolias@gmail.com>
2024-04-17 21:58:34 +0300

    carousel works fine

--- wwwroot/js/carousel.js
+++ wwwroot/js/carousel.js
@@ -3,14 +3,21 @@ const images = document.querySelectorAll('.carousel img');
 
 // Apply initial styles and animation delay
 images.forEach((img, index) => {
-    img.style.animation = `carousel-animation 4s ${index * 4}s infinite`; // Change '4' to the duration of your animation
+    img.style.animation = `carousel-animation 4s ${index * 4}s`;
 });
 
 function runCarousel() {
+    // Reset all images
+    for(let i = 0; i < images.length; i++) {
+        images[i].style.animation = '';
+        void images[i].offsetWidth; // Trigger reflow to restart animation
+        images[i].style.animation = `carousel-animation 4s ${i * 4}s`;
+    }
+
     currentIndex++;
     if(currentIndex >= images.length) {
         currentIndex = 0;
     }
 }
 
-setInterval(runCarousel, 4000); // Change image every 4 seconds
+setInterval(runCarousel, images.length * 4000); // Change image every 4 seconds times the number of images
