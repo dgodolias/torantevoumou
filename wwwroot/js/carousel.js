c00e83eab7f7bee2ca7071f9ed059640f2dcf3be
dgodolias <dgodolias@gmail.com>
2024-04-17 21:44:56 +0300

    i made a carousel(not working fully)

--- wwwroot/js/carousel.js
+++ wwwroot/js/carousel.js
@@ -2,11 +2,17 @@ var images = document.querySelectorAll('.carousel-image');
 var currentImage = 0;
 
 // Show the first image
-images[currentImage].style.display = 'block';
+images[currentImage].classList.add('show');
 
-// Change the image every 3 seconds
+// Change the image every 4 seconds
 setInterval(function() {
-    images[currentImage].style.display = 'none';
+    images[currentImage].classList.remove('show', 'carousel-image');
+    void images[currentImage].offsetWidth;
+    images[currentImage].classList.add('carousel-image');
+
     currentImage = (currentImage + 1) % images.length;
-    images[currentImage].style.display = 'block';
-}, 3000);
+
+    images[currentImage].classList.remove('carousel-image');
+    void images[currentImage].offsetWidth;
+    images[currentImage].classList.add('show', 'carousel-image');
+}, 4000); /* Increase the interval to 4 seconds */
