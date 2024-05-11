4b0f16946f66c6df986d68e3f092a71eecc69c7b
dgodolias <dgodolias@gmail.com>
2024-05-11 15:17:36 +0300

    seperated js css and html code in alphindex

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -20,10 +20,6 @@ function displayBusinesses() {
     // Display businesses
     var businessDictionary = document.getElementById('businessDictionary');
     businessDictionary.innerHTML = '';
-    businessDictionary.style.border = '1px solid black';
-    businessDictionary.style.padding = '10px';
-    businessDictionary.style.width = '200px';
-    businessDictionary.style.columnCount = '2';
     for (var i = 0; i < alphabet.length; i++) {
         var letterDiv = document.createElement('div');
         letterDiv.style.borderBottom = '1px solid black';
@@ -60,62 +56,3 @@ function filterBusinesses() {
 
 // Display businesses when the page loads
 displayBusinesses();
-
-// Create a parent container
-var parentContainer = document.createElement('div');
-parentContainer.style.display = 'flex';
-
-// Get the businessDictionary and append it to the parent container
-var businessDictionary = document.getElementById('businessDictionary');
-parentContainer.appendChild(businessDictionary);
-
-// Create a new box for the search bar
-var searchBarContainer = document.createElement('div');
-searchBarContainer.style.border = '1px solid black';
-searchBarContainer.style.padding = '10px';
-searchBarContainer.style.flexGrow = '1'; // This will make the searchBarContainer take up the remaining space
-searchBarContainer.style.display = 'flex';
-searchBarContainer.style.flexDirection = 'column';
-searchBarContainer.style.alignItems = 'center';
-
-// Create a container for the title and the search bar
-var titleSearchBarContainer = document.createElement('div');
-titleSearchBarContainer.style.display = 'flex';
-titleSearchBarContainer.style.justifyContent = 'center'; // This will distribute the available space around the items
-titleSearchBarContainer.style.width = '100%';
-
-// Create a title
-var title = document.createElement('h2');
-title.id = 'alphindex'; // Add an id to the title
-title.textContent = 'Οι υπηρεσίες μας';
-title.style.marginRight = '0px'; // Remove the margin to the right of the title
-title.style.marginBottom = '0px'; // Remove the margin to the bottom of the title
-
-// Create a spacer element
-var spacer = document.createElement('div');
-spacer.style.width = '30px'; // Set the width of the spacer to 30px
-
-// Move the search bar to the new box
-var searchBar = document.getElementById('searchBar');
-
-// Add the title, spacer and search bar to the titleSearchBarContainer
-titleSearchBarContainer.appendChild(title);
-titleSearchBarContainer.appendChild(spacer);
-titleSearchBarContainer.appendChild(searchBar);
-
-// Create a separation line
-var line = document.createElement('hr');
-line.style.width = '50%';
-line.style.borderTop = '1px solid black';
-line.style.alignSelf = 'center';
-
-// Add the titleSearchBarContainer and line to the searchBarContainer
-searchBarContainer.appendChild(titleSearchBarContainer);
-searchBarContainer.appendChild(line);
-
-// Add the new box to the parent container
-parentContainer.appendChild(searchBarContainer);
-
-// Add the parent container to the container
-var container = document.querySelector('#services .container');
-container.appendChild(parentContainer);
