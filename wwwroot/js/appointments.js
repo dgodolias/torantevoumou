4c7133a952b61bb4c35c4faf995951199378639e
dgodolias <dgodolias@gmail.com>
2024-05-31 20:00:22 +0300

    made the list in appointmets

--- wwwroot/js/appointments.js
+++ wwwroot/js/appointments.js
@@ -1,44 +1,7 @@
-var globalAppointments = [];
-
 $(document).ready(function() {
-    $('#serviceSelect').change(function() {
-        var selectedService = $(this).val();
-        if (selectedService) {
-            // Make an AJAX request to the server with the selected service
-            $.ajax({
-                url: '/api/SetSelectedService',
-                type: 'POST',
-                headers: { 'RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val() },
-                contentType: 'application/json',
-                data: JSON.stringify(selectedService),
-                success: function(response) {
-                    if (response.success) {
-                        $('#selectedService').val(selectedService);
-                        $('#datepickerappointments').show();
-
-                        // Make a GET request to get the list of service appointments
-                        $.ajax({
-                            url: '/api/GetServiceAppointments/' + selectedService,
-                            type: 'GET',
-                            success: function(appointments) {
-                                // Store appointments in the global variable
-                                globalAppointments = appointments;
-
-                                // Print each appointment
-                                appointments.forEach(function(appointment) {
-                                    console.log("Appointment: ", appointment);
-                                });
-                            },
-                            error: function(error) {
-                                console.error("Error getting appointments: ", error);
-                            }
-                        });
-                    }
-                }
-            });
-            console.log(selectedService);
-        } else {
-            $('#datepickerappointments').hide();
-        }
+    $('#serviceList .serviceItem').on('click', function() {
+        var serviceName = $(this).attr('id');
+        console.log('You clicked on service: ' + serviceName);
+        // Do something with the service name
     });
 });
