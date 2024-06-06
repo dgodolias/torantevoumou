520215a4c9219d01ad5b41edac99afa4c836d1ea
dgodolias <dgodolias@gmail.com>
2024-06-06 23:37:45 +0300

    made the profile functional again

--- obj/Debug/net9.0/torantevoumou.AssemblyInfo.cs
+++ obj/Debug/net9.0/torantevoumou.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("torantevoumou")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65d537ddbae2a966a761974e6e34d7c719508d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dc792c9c3722e901251a92e44d306eb2f400e13")]
 [assembly: System.Reflection.AssemblyProductAttribute("torantevoumou")]
 [assembly: System.Reflection.AssemblyTitleAttribute("torantevoumou")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
