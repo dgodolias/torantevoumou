bfcd0403f1b16ac1d54041a397cebb8120a93a52
dgodolias <dgodolias@gmail.com>
2024-06-18 13:07:30 +0300

    dialog appointments is 100% complete, moving on to fixing the appointments page and past appointments page

--- obj/Debug/net9.0/torantevoumou.AssemblyInfo.cs
+++ obj/Debug/net9.0/torantevoumou.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("torantevoumou")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e540a58874ae448b278fcf0321a53fbaf3a1cf19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33729bdb06229b709cefa5b1a1f97d9ab7c87127")]
 [assembly: System.Reflection.AssemblyProductAttribute("torantevoumou")]
 [assembly: System.Reflection.AssemblyTitleAttribute("torantevoumou")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
